f74010f1a1e3218513abdc177e3ab0f938a584e9
dzakwandaffap <dzakwan.d.pratama@gmail.com>
2025-08-21 17:33:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",           // file HTML utama
+    "./src/**/*.{js,ts,jsx,tsx}" // semua file React di src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
